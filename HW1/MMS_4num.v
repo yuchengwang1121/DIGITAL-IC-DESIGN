3887f77e6aa8cd5896df9e162eedd780356e6ace
YuChengWang <brian0978599918@gmail.com>
2023-03-13 21:07:28 +0800

    Upload HW1 at 03/13

---
+++ HW1/MMS_4num.v
@@ -0,0 +1,14 @@
+module MMS_4num(result, select, number0, number1, number2, number3);
+
+input        select;
+input  [7:0] number0;
+input  [7:0] number1;
+input  [7:0] number2;
+input  [7:0] number3;
+output [7:0] result; 
+
+/*
+	Write Your Design Here ~
+*/
+
+endmodule
---
+++ HW1/MMS_8num.v
@@ -0,0 +1,19 @@
+
+module MMS_8num(result, select, number0, number1, number2, number3, number4, number5, number6, number7);
+
+input        select;
+input  [7:0] number0;
+input  [7:0] number1;
+input  [7:0] number2;
+input  [7:0] number3;
+input  [7:0] number4;
+input  [7:0] number5;
+input  [7:0] number6;
+input  [7:0] number7;
+output [7:0] result; 
+
+/*
+	Write Your Design Here ~
+*/
+
+endmodule
